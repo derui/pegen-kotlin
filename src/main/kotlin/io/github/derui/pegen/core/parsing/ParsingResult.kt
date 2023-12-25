97c1c15d8155ec891f28aa825c3a73ac1ab23d8e
derui <derutakayu@gmail.com>
2023-12-26 08:08:48 +0900

    feat: add context for parser

--- src/main/kotlin/io/github/derui/pegen/core/parsing/ParsingResult.kt
+++ src/main/kotlin/io/github/derui/pegen/core/parsing/ParsingResult.kt
@@ -1,21 +1,40 @@
 package io.github.derui.pegen.core.parsing
 
+import io.github.derui.pegen.core.support.Err
+import io.github.derui.pegen.core.support.Ok
 import io.github.derui.pegen.core.support.Result
 
 /**
  * A type of parsing result
  */
-sealed interface ParsingResult<V>
+sealed interface ParsingResult<V> {
+    companion object {
+        /**
+         * Create a [Parsed] instance.
+         */
+        fun <V> parsed(result: V): ParsingResult<V> = Parsed(Ok(result))
+
+        /**
+         * Create a [Parsed] instance.
+         */
+        fun <V> parsed(result: ErrorInfo): ParsingResult<V> = Parsed(Err(result))
+
+        /**
+         * Create a [NoParse] instance.
+         */
+        fun <V> noParse(): ParsingResult<V> = NoParse()
+    }
+}
 
 /**
  * [Parsed] holds a result of parse of an expression.
  */
-data class Parsed<V>(val result: Result<V, ErrorInfo>) : ParsingResult<V>
+class Parsed<V> internal constructor(val result: Result<V, ErrorInfo>) : ParsingResult<V>
 
 /**
  * [NoParse] is marker class for that did not parse yet.
  */
-class NoParse<V> : ParsingResult<V> {
+class NoParse<V> internal constructor() : ParsingResult<V> {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
