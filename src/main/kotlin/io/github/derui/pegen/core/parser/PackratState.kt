eb21d7f878c63679296cfabd3a49aeebf9d9da65
derui <derutakayu@gmail.com>
2024-01-01 10:40:06 +0900

    feat: add cache with packrat state

--- src/main/kotlin/io/github/derui/pegen/core/parser/PackratState.kt
+++ src/main/kotlin/io/github/derui/pegen/core/parser/PackratState.kt
@@ -9,25 +9,55 @@ import java.util.UUID
  *
  * This class is NOT thread-safe.
  */
-class PackratState<V, TagType> private constructor(
-    private val cache: Array<MutableMap<UUID, ParserResultCache<V>>>,
-    private val expressions: Map<UUID, PegDefinition<V, TagType>>,
+class PackratState<V> private constructor(
+    private val cache: Array<MutableMap<UUID, ParserResultCache<ParsingResult<V>>>>,
 ) {
     companion object {
         fun <V, TagType> from(
             input: String,
             expressions: List<PegDefinition<V, TagType>>,
-        ): PackratState<V, TagType> {
+        ): PackratState<V> {
             val expMap = expressions.associateBy { it.id }
 
             return PackratState(
                 Array(input.length) {
                     expMap.mapValues {
-                        ParserResultCache.NoParse<V>()
+                        ParserResultCache.NoParse<ParsingResult<V>>()
                     }.toMutableMap()
                 },
-                expMap,
             )
         }
+    }
+
+    /**
+     * get cache for [syntaxId] at [index]
+     */
+    fun get(
+        syntaxId: UUID,
+        index: Int,
+    ): ParserResultCache<ParsingResult<V>> {
+        return cache[index][syntaxId] ?: ParserResultCache.NoParse()
+    }
+
+    /**
+     * put parsing result with parsed result
+     */
+    fun putParsed(
+        syntaxId: UUID,
+        index: Int,
+        result: ParsingResult<V>,
+    ) {
+        cache[index][syntaxId] = ParserResultCache.parsed(result)
+    }
+
+    /**
+     * put parsing result with error
+     */
+    fun putError(
+        syntaxId: UUID,
+        index: Int,
+        error: ErrorInfo,
+    ) {
+        cache[index][syntaxId] = ParserResultCache.parsed(error)
     }
 }
