ea32a0c79bc9d625d6b4086fdaeeada5ec07d6c9
derui <derutakayu@gmail.com>
2024-01-03 15:03:24 +0900

    feat: add sample language with pegen

---
+++ pegen-kotlin-sample/src/main/kotlin/Language.kt
@@ -0,0 +1,59 @@
+package io.github
+
+import io.github.derui.pegen.core.Pegen
+
+sealed class Node
+
+class Expr(val value: Long) : Node() {
+    companion object : Pegen.Def<Node, String>(
+        Pegen<Node, String>() define {
+            Sum() tagged "sum"
+        } constructAs {
+            Expr(it.tagged("sum")?.asType<Sum>()?.value ?: 0L)
+        },
+    )
+}
+
+class Sum(val value: Long) : Node() {
+    companion object : Pegen.Def<Node, String>(
+        Pegen<Node, String>() define {
+            s(Product(), many(g(s(g(+"+" / +"-"), Product()))))
+        } constructAs {
+            val initial = it.tagged("initial")?.asType<Product>()?.value ?: 0L
+            val values =
+                it.tagged("value")?.asType<List<Pair<String, Product>>>()?.map { it.second.value * if (it.first == "+") 1 else -1 }
+                    ?: emptyList()
+            TODO()
+        },
+    )
+}
+
+class Product(val value: Long) : Node() {
+    companion object : Pegen.Def<Node, String>(
+        Pegen<Node, String>().define {
+            s(Power(), many(g(s(g(+"*" / +"/"), Power()))))
+        } constructAs {
+            TODO()
+        },
+    )
+}
+
+class Power(val value: Long) : Node() {
+    companion object : Pegen.Def<Node, String>(
+        Pegen<Node, String>().define {
+            s(Value(), opt(s(+"^", Power())))
+        } constructAs {
+            TODO()
+        },
+    )
+}
+
+class Value(val value: Long) : Node() {
+    companion object : Pegen.Def<Node, String>(
+        Pegen<Node, String>().define {
+            cls { '0'..'9' } / s(+"(", Expr(), +")")
+        } constructAs {
+            TODO()
+        },
+    )
+}
